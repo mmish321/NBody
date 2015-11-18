d48733e2edb36eb072cdb92fb2f523246dc5a1d9
Michelle Mao <maom@s.dcsdk12.org>
2015-11-18 11:06:14 -0700

    Finish

--- planets.rb
+++ planets.rb
@@ -1,14 +1,29 @@
+
 class Planets
-	def initialize(x,y,xvel,yvel,mass,image)
+	G = 6.67408e-11
+	attr_accessor :x, :y, :xvel, :yvel,:mass,:total_force, :name
+	def initialize(x,y,xvel,yvel,mass,img,name,radius)
 		@x = x
-		@y =y 
+		@y = y
+		@xscaled =(x /(radius *2)) + 250
+		@yscaled =(y/radius) + 250
 		@xvel = xvel
 		@yvel = yvel
 		@mass = mass
-		#@image = Gosu::Image.new("images/#{img}")
+		@img = Gosu::Image.new("images/#{img}")
+		@radius = radius
+		@total_force = 0
+		@name = name
 	end
+	
 	def draw
-		
+		@img.draw(@xscaled - @img.width / 2.0, @yscaled - @img.height / 2.0 ,1)
+	end
+		def calculate_gravity(planet)
+		xdist =(x - planet.y).abs
+		ydist = (y - planet.y).abs
+		dist = Math.sqrt(xdist**2 + ydist**2)
+		@total_force += ((G*mass*planet.mass)/dist**2)
 	end
 
 
--- universe.rb
+++ universe.rb
@@ -3,7 +3,8 @@ require_relative "planets"
 
 
 class Universe < Gosu::Window
-	attr_accessor :radius
+	G = 6.67408e-11
+	attr_accessor :radius ,:G
 	def initialize
 		super 500, 500
 		self.caption = "Universe, heck yeah"
@@ -11,25 +12,41 @@ class Universe < Gosu::Window
 		data=File.read("planets.txt")
 		lines = data.split("\n")
 		@number_of_planets = lines[0]
-		@radius = lines[1]
+		@bigradius = lines[1].to_f
+		@radius = (@bigradius/500).to_f
 		@planets_data = lines[2...lines.length]
 		@planets = []
 		@planets_data.each {|planet| 
-			planet = planet.split(",")
-			@planets.push(Planets.new(planet[0].to_f,planet[1].to_f,planet[2].to_f,planet[3].to_f,planet[4].to_f,planet[5]))
+			planet = planet.split(" ")
+			@planets.push(Planets.new(planet[0].to_f,planet[1].to_f,planet[2].to_f,planet[3].to_f,planet[4].to_f,planet[5],planet[6],radius.to_f))
 		}
 	end
 
 	def update
-		
+		@planets.each do |planet|
+			for i in 0...@planets.length
+				if @planets[i] != planet.name
+				planet.calculate_gravity(@planets[i])
+				end
+			end
+		end
 	end
-
+	def ok
+		@planets.each do |planet|
+			puts planet.total_force
+		end
+	end
 	def draw
 		@background_image.draw(0,0,0)
+		@planets.each do |planet|
+			planet.draw
+		end
 	end
+
 
 
 end
 
 window = Universe.new
 window.show
+window.ok
